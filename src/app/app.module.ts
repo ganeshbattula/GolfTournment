7401e01e8d8b51550d40ff050717def4c04918d7
Ganesh Battula <ganesh990877@gmail.com.com>
2023-04-30 17:48:48 +0100

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlayerTableComponent } from './player-table/player-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayerTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
